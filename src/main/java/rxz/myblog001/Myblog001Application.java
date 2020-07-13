1bb94443a7d8b4e4f409d26eddf948c396696750
rxz <renx7250@163.com>
2020-07-13 19:57:15 +0800

    初始化项目

---
+++ src/main/java/rxz/myblog001/Myblog001Application.java
@@ -0,0 +1,13 @@
+package rxz.myblog001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Myblog001Application.class, args);
+    }
+
+}
---
+++ src/test/java/rxz/myblog001/Myblog001ApplicationTests.java
@@ -0,0 +1,13 @@
+package rxz.myblog001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myblog001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
